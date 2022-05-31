fc2bf06efb62e3b35f352aff6e503113d3c5cdd6
Rahul Kumar <54908639+Purpledevil27@users.noreply.github.com>
2022-05-31 18:03:14 +0530

    Create 1462C - Unique Number.cpp

---
+++ Codeforces/1462C - Unique Number.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x;
+        cin >> x;
+        int ans = 0;
+        if (x > 45)
+        {
+            cout << -1 << endl;
+            continue;
+        }
+        int p = 9;
+        while (x > 0 && x)
+        {
+            if (x < 10 && x < p)
+            {
+                break;
+            }
+            ans += round(pow(10, 9 - p) * p);
+            x -= p--;
+        }
+        ans += pow(10, 9 - p) * x;
+        cout << ans << endl;
+    }
+    return 0;
+}
