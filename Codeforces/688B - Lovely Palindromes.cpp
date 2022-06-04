c865318dff8836ab1c170fba2007be04ad70f4f2
Rahul Kumar <54908639+Purpledevil27@users.noreply.github.com>
2022-06-04 19:30:09 +0530

    Create 688B - Lovely Palindromes.cpp

---
+++ Codeforces/688B - Lovely Palindromes.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    string ans = s;
+    reverse(s.begin(), s.end());
+    ans = ans + s;
+    cout << ans;
+    return 0;
+}
