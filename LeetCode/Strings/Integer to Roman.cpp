e6fc82cf4ec1b8622b6be0d46605f2c5564e162a
Rahul Kumar <54908639+Purpledevil27@users.noreply.github.com>
2022-06-12 17:51:14 +0530

    Update Integer to Roman.cpp

--- LeetCode/Strings/Integer to Roman.cpp
+++ LeetCode/Strings/Integer to Roman.cpp
@@ -49,6 +49,20 @@ class Solution
 public:
     string intToRoman(int num)
     {
+        string ones[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        string tens[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        string hrns[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        string ths[] = {"", "M", "MM", "MMM"};
+
+        return ths[num / 1000] + hrns[(num % 1000) / 100] + tens[(num % 100) / 10] + ones[num % 10];
+    }
+};
+
+class Solution
+{
+public:
+    string intToRoman(int num)
+    {
         string snum = to_string(num), ans = "";
         int len = snum.length();
         for (int i = 0; i < len; i++)
