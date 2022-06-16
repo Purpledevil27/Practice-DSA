262a6fb44e962f9b3e6be605235daa6452850d6f
Rahul Kumar <54908639+Purpledevil27@users.noreply.github.com>
2022-06-17 00:12:00 +0530

    Create 1694B - Paranoid String.cpp

---
+++ Codeforces/1694B - Paranoid String.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, ans;
+        string S;
+        cin >> n >> S;
+        ans = n;
+        for (int i = 1; i < n; ++i)
+            if (S[i] != S[i - 1])
+                ans += i;
+        cout << ans << '\n';
+    }
+    return 0;
+}
