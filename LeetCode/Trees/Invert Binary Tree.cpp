77aebf13f0bf5266f8bff843f7b1346064ec47ca
Rahul Kumar <54908639+Purpledevil27@users.noreply.github.com>
2022-06-21 18:45:22 +0530

    Create Invert Binary Tree.cpp

---
+++ LeetCode/Trees/Invert Binary Tree.cpp
@@ -0,0 +1,168 @@
+// https://leetcode.com/problems/invert-binary-tree/
+
+/*Given the root of a binary tree, invert the tree, and return its root.
+
+Example 1:
+
+
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+
+Example 2:
+
+Input: root = [2,1,3]
+Output: [2,3,1]
+
+Example 3:
+
+Input: root = []
+Output: []
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *invertTree(TreeNode *root)
+    {
+        if (root == NULL)
+            return root;
+        queue<TreeNode *> q;
+        q.push(root);
+        while (!q.empty())
+        {
+            TreeNode *front = q.front();
+            q.pop();
+
+            TreeNode *temp = front->left;
+            front->left = front->right;
+            front->right = temp;
+
+            if (front->left)
+            {
+                q.push(front->left);
+            }
+            if (front->right)
+            {
+                q.push(front->right);
+            }
+        }
+        return root;
+    }
+};
+
+class Solution
+{
+public:
+    void solve(TreeNode *root)
+    {
+        if (root == NULL)
+        {
+            return;
+        }
+        TreeNode *left = invertTree(root->left);
+        TreeNode *right = invertTree(root->right);
+        TreeNode *temp = right;
+        root->right = left;
+        root->left = temp;
+    }
+    TreeNode *invertTree(TreeNode *root)
+    {
+        solve(root);
+        return root;
+    }
+};
+TreeNode *takeInputLevelWise()
+{
+    queue<TreeNode *> pendingNodes;
+    int rootData;
+    cout << "enter root data:";
+    cin >> rootData;
+    if (rootData == -1)
+    {
+        return NULL;
+    }
+
+    TreeNode *root = new TreeNode(rootData);
+    pendingNodes.push(root);
+    while (!pendingNodes.empty())
+    {
+        TreeNode *front = pendingNodes.front();
+        pendingNodes.pop();
+        cout << "Enter left child of " << front->val << endl;
+        int leftinput;
+        cin >> leftinput;
+        if (leftinput != -1)
+        {
+            TreeNode *leftchild = new TreeNode(leftinput);
+            front->left = leftchild;
+            pendingNodes.push(leftchild);
+        }
+        cout << "Enter right child of " << front->val << endl;
+        int rightinput;
+        cin >> rightinput;
+        if (rightinput != -1)
+        {
+            TreeNode *rightchild = new TreeNode(rightinput);
+            front->right = rightchild;
+            pendingNodes.push(rightchild);
+        }
+    }
+    return root;
+}
+
+void printTreeLevelWise(TreeNode *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    queue<TreeNode *> pendingNodes;
+    pendingNodes.push(root);
+    while (!pendingNodes.empty())
+    {
+        TreeNode *front = pendingNodes.front();
+        cout << front->val << ":";
+        pendingNodes.pop();
+        if (front->left != NULL)
+        {
+            cout << "L" << front->left->val;
+            pendingNodes.push(front->left);
+        }
+
+        if (front->right != NULL)
+        {
+            cout << "R" << front->right->val;
+            pendingNodes.push(front->right);
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    TreeNode *root = takeInputLevelWise();
+    Solution obj;
+    root = obj.invertTree(root);
+    printTreeLevelWise(root);
+    return 0;
+}
