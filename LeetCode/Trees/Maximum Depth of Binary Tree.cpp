00f95fe4c0d02800dc88da1142068e1565775fbb
Rahul Kumar <54908639+Purpledevil27@users.noreply.github.com>
2022-06-22 12:11:40 +0530

    Create Maximum Depth of Binary Tree.cpp

---
+++ LeetCode/Trees/Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,126 @@
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+/*Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Example 1:
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+
+Example 2:
+
+Input: root = [1,null,2]
+Output: 2
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 10^4].
+-100 <= Node.val <= 100
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    int maxDepth(TreeNode *root)
+    {
+        if (root == NULL)
+        {
+            return 0;
+        }
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
+
+// Driver Code
+TreeNode *takeInputLevelWise()
+{
+    queue<TreeNode *> pendingNodes;
+    int rootData;
+    cout << "enter root data:";
+    cin >> rootData;
+    if (rootData == -1)
+    {
+        return NULL;
+    }
+
+    TreeNode *root = new TreeNode(rootData);
+    pendingNodes.push(root);
+    while (!pendingNodes.empty())
+    {
+        TreeNode *front = pendingNodes.front();
+        pendingNodes.pop();
+        cout << "Enter left child of " << front->val << endl;
+        int leftinput;
+        cin >> leftinput;
+        if (leftinput != -1)
+        {
+            TreeNode *leftchild = new TreeNode(leftinput);
+            front->left = leftchild;
+            pendingNodes.push(leftchild);
+        }
+        cout << "Enter right child of " << front->val << endl;
+        int rightinput;
+        cin >> rightinput;
+        if (rightinput != -1)
+        {
+            TreeNode *rightchild = new TreeNode(rightinput);
+            front->right = rightchild;
+            pendingNodes.push(rightchild);
+        }
+    }
+    return root;
+}
+
+void printTreeLevelWise(TreeNode *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    queue<TreeNode *> pendingNodes;
+    pendingNodes.push(root);
+    while (!pendingNodes.empty())
+    {
+        TreeNode *front = pendingNodes.front();
+        cout << front->val << ":";
+        pendingNodes.pop();
+        if (front->left != NULL)
+        {
+            cout << "L" << front->left->val;
+            pendingNodes.push(front->left);
+        }
+
+        if (front->right != NULL)
+        {
+            cout << "R" << front->right->val;
+            pendingNodes.push(front->right);
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    TreeNode *root = takeInputLevelWise();
+    Solution obj;
+    cout << obj.maxDepth(root);
+    // printTreeLevelWise(root);
+    return 0;
+}
