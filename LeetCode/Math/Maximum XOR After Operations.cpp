fc2329570209b20768c1bae75a9cc24011a44825
Rahul Kumar <54908639+Purpledevil27@users.noreply.github.com>
2022-07-05 17:36:06 +0530

    Update Maximum XOR After Operations.cpp

--- LeetCode/Math/Maximum XOR After Operations.cpp
+++ LeetCode/Math/Maximum XOR After Operations.cpp
@@ -1,6 +1,7 @@
 // https://leetcode.com/problems/maximum-xor-after-operations/
 
-/*You are given a 0-indexed integer array nums. In one operation, select any non-negative integer x and an index i, then update nums[i] to be equal to nums[i] AND (nums[i] XOR x).
+/*You are given a 0-indexed integer array nums. In one operation, select any non-negative integer x and an index i, then update
+nums[i] to be equal to nums[i] AND (nums[i] XOR x).
 
 Note that AND is the bitwise AND operation and XOR is the bitwise XOR operation.
 
