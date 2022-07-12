82f8bd0aa065b583dc63259c6a47a3b08aa92ee3
Rahul Kumar <54908639+Purpledevil27@users.noreply.github.com>
2022-07-13 00:01:31 +0530

    Update 1703F - Yet Another Problem About Pairs Satisfying an Inequality.cpp

--- Codeforces/1703F - Yet Another Problem About Pairs Satisfying an Inequality.cpp
+++ Codeforces/1703F - Yet Another Problem About Pairs Satisfying an Inequality.cpp
@@ -1,3 +1,4 @@
+// using upper_bound - Approach #1
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -38,6 +39,37 @@ int main()
             count += (v.size() - u);
         }
         cout << count << endl;
+    }
+    return 0;
+}
+
+// Using Prefix-sum - Approach #2
+#include <bits/stdc++.h>
+
+using namespace std;
+int t, n, a[200005], pref[200005];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin >> t;
+    while (t--)
+    {
+        cin >> n;
+        long long ans = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            cin >> a[i];
+            pref[i] = pref[i - 1] + (a[i] < i);
+            // cout << pref[i] << " ";
+            if (a[i] < i)
+            {
+                cout << i << " " << a[i] << " " << pref[a[i] - 1] << " " << endl;
+                ans += pref[a[i] - 1];
+            }
+        }
+
+        cout << "\n"
+             << ans << "\n";
     }
     return 0;
 }
