18369ff4c2daa18fe12a28e262f47aab25252f93
Rahul Kumar <54908639+Purpledevil27@users.noreply.github.com>
2022-07-12 23:28:01 +0530

    Update 1703D - Double Strings.cpp

--- Codeforces/1703D - Double Strings.cpp
+++ Codeforces/1703D - Double Strings.cpp
@@ -1,3 +1,4 @@
+// Using Tries - Approach #1
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -124,4 +125,51 @@ int main()
         cout << ans << endl;
     }
     return 0;
+}
+
+// Using Hashmaps - Approach #2
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAX = 200007;
+const int MOD = 1000000007;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    string s[n];
+    map<string, bool> mp;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> s[i];
+        mp[s[i]] = true;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        bool ok = false;
+        for (int j = 1; j < s[i].length(); j++)
+        {
+            string pref = s[i].substr(0, j), suff = s[i].substr(j, s[i].length() - j);
+            if (mp[pref] && mp[suff])
+            {
+                ok = true;
+            }
+        }
+        cout << ok;
+    }
+    cout << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tt;
+    cin >> tt;
+    for (int i = 1; i <= tt; i++)
+    {
+        solve();
+    }
+    // solve();
 }
