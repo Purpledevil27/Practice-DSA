848b86a5895ce45da71672c7c678704fa09cce18
Rahul Kumar <54908639+Purpledevil27@users.noreply.github.com>
2022-07-17 18:52:52 +0530

    Update Smallest Positive missing number.cpp

--- GeeksForGeeks/Arrays/Smallest Positive missing number.cpp
+++ GeeksForGeeks/Arrays/Smallest Positive missing number.cpp
@@ -8,16 +8,13 @@ Input:
 N = 5
 arr[] = {1,2,3,4,5}
 Output: 6
-Explanation: Smallest positive missing
-number is 6.
+Explanation: Smallest positive missing number is 6.
+
 Example 2:
 
-Input:
-N = 5
-arr[] = {0,-10,1,3,-20}
+Input: N = 5 arr[] = {0,-10,1,3,-20}
 Output: 2
-Explanation: Smallest positive missing
-number is 2.
+Explanation: Smallest positive missing number is 2.
 Your Task:
 The task is to complete the function missingNumber() which returns the smallest positive missing number in the array.
 
