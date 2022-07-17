793eefd74a007d0c3e078a75e8bf75164f2155c8
Rahul Kumar <54908639+Purpledevil27@users.noreply.github.com>
2022-07-17 18:57:46 +0530

    Update Largest subarray with 0 sum.cpp

--- GeeksForGeeks/Arrays/Largest subarray with 0 sum.cpp
+++ GeeksForGeeks/Arrays/Largest subarray with 0 sum.cpp
@@ -4,12 +4,9 @@
 
 Example 1:
 
-Input:
-N = 8
-A[] = {15,-2,2,-8,1,7,10,23}
+Input: N = 8 A[] = {15,-2,2,-8,1,7,10,23}
 Output: 5
-Explanation: The largest subarray with
-sum 0 will be -2 2 -8 1 7.
+Explanation: The largest subarray with sum 0 will be -2 2 -8 1 7.
 Your Task:
 You just have to complete the function maxLen() which takes two arguments an array A and n, where n is the size of the array A and returns the length of the largest subarray with 0 sum.
 
@@ -17,9 +14,8 @@ Expected Time Complexity: O(N).
 Expected Auxiliary Space: O(N).
 
 Constraints:
-1 <= N <= 105
+1 <= N <= 10^5
 -1000 <= A[i] <= 1000, for each valid i
-
 */
 
 //{ Driver Code Starts
