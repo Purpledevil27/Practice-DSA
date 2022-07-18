70c02b206d10993c2dedf0c453324046c71bb4fb
Rahul Kumar <54908639+Purpledevil27@users.noreply.github.com>
2022-07-18 20:56:43 +0530

    Update Rotate Image.cpp

--- LeetCode/Matrix/Rotate Image.cpp
+++ LeetCode/Matrix/Rotate Image.cpp
@@ -107,3 +107,18 @@ public:
         }
     }
 };
+
+// Approach #4
+class Solution
+{
+public:
+    void rotate(vector<vector<int>> &mat)
+    {
+        int n = mat.size();
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j < n; j++) // first swap then reverse each row
+                swap(mat[i][j], mat[j][i]);
+        for (int i = 0; i < n; i++)
+            reverse(mat[i].begin(), mat[i].end());
+    }
+};
