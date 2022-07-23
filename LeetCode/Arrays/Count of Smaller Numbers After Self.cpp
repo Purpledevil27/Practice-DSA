94537b8296a1a45d18a84ead7c9855e3c089e872
Rahul Kumar <54908639+Purpledevil27@users.noreply.github.com>
2022-07-23 18:10:43 +0530

    Update Count of Smaller Numbers After Self.cpp

--- LeetCode/Arrays/Count of Smaller Numbers After Self.cpp
+++ LeetCode/Arrays/Count of Smaller Numbers After Self.cpp
@@ -3,8 +3,6 @@
 
 /*You are given an integer array nums and you have to return a new counts array. The counts array has the property where counts[i] is the number of smaller elements to the right of nums[i].
 
-
-
 Example 1:
 
 Input: nums = [5,2,6,1]
