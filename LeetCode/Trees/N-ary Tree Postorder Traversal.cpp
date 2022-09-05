79252cb7481197dc0939dcef4358faf9296dee54
Rahul Kumar <54908639+Purpledevil27@users.noreply.github.com>
2022-09-05 23:45:29 +0530

    Add files

---
+++ LeetCode/Trees/N-ary Tree Postorder Traversal.cpp
@@ -0,0 +1,97 @@
+// https://leetcode.com/problems/n-ary-tree-postorder-traversal/
+
+/*Given the root of an n-ary tree, return the postorder traversal of its nodes' values.
+
+Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
+
+Example 1:
+
+Input: root = [1,null,3,2,4,null,5,6]
+Output: [5,6,3,2,4,1]
+
+Example 2:
+
+Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+Output: [2,6,14,11,7,3,12,8,4,13,9,10,5,1]
+
+Constraints:
+The number of nodes in the tree is in the range [0, 10^4].
+0 <= Node.val <= 10^4
+The height of the n-ary tree is less than or equal to 1000.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a Node.
+class Node
+{
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+};
+
+// Iteratively
+class Solution
+{
+public:
+    vector<int> postorder(Node *root)
+    {
+        vector<int> ans;
+        if (root == NULL)
+        {
+            return ans;
+        }
+        stack<Node *> s;
+        s.push(root);
+        while (!s.empty())
+        {
+            Node *top = s.top();
+            s.pop();
+
+            for (int i = 0; i < top->children.size(); i++)
+            {
+                s.push(top->children[i]);
+            }
+            ans.push_back(top->val);
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+// Recursively
+class Solution
+{
+    void find(Node *root, vector<int> &ans)
+    {
+        if (root == NULL)
+            return;
+        for (auto &i : root->children)
+        {
+            find(i, ans);
+        }
+        ans.push_back(root->val);
+    }
+
+public:
+    vector<int> postorder(Node *root)
+    {
+        vector<int> ans;
+        find(root, ans);
+        return ans;
+    }
+};
